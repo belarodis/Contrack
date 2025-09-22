0e478a2b6f5eb1631356eb862732c2a3bb615ce9
HMarchiori <hmarchiori2003@gmail.com>
2025-09-22 18:24:07 -0300

    feat: add Mapper classes by MapStruct.

---
+++ backend/contrack-app/src/main/java/com/contrack/contrack_app/mapper/PessoaMapper.java
@@ -0,0 +1,29 @@
+package com.contrack.contrack_app.mapper;
+
+import com.contrack.contrack_app.dto.create.PessoaCreateDTO;
+import com.contrack.contrack_app.dto.view.PessoaViewDTO;
+import com.contrack.contrack_app.mapper.config.MapStructCentralConfig;
+import com.contrack.contrack_app.models.Pessoa;
+import org.mapstruct.*;
+
+@Mapper(config = MapStructCentralConfig.class)
+public interface PessoaMapper {
+
+    // VIEW
+    PessoaViewDTO toView(Pessoa entity);
+
+    // CREATE -> ENTITY
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "contratos", ignore = true)
+    @Mapping(target = "alocacoes", ignore = true)
+    Pessoa toEntity(PessoaCreateDTO dto);
+
+    // helper
+    @Named("pessoaFromId")
+    default Pessoa fromId(Long id) {
+        if (id == null) return null;
+        Pessoa p = new Pessoa();
+        p.setId(id);
+        return p;
+    }
+}
