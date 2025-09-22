1694b2d5483c238c66e41a06efa2e77d4e2103a8
Isabela Rodrigues de Souza <bela.rodjj@gmail.com>
2025-09-22 15:39:44 -0300

    estrutura base

---
+++ backend/contrack-app/src/main/java/com/contrack/contrack_app/ContrackAppApplication.java
@@ -0,0 +1,13 @@
+package com.contrack.contrack_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContrackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContrackAppApplication.class, args);
+	}
+
+}
