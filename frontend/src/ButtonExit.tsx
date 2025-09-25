e7fddb62620e26e0eba7d0bedd8f1c6b57da50a5
HMarchiori <hmarchiori2003@gmail.com>
2025-09-24 22:00:37 -0300

    FEAT: Add Buttons.

---
+++ frontend/src/ButtonExit.tsx
@@ -0,0 +1,9 @@
+function ButtonExit() {
+    return (
+        <div className="flex h-fit bg-[#6e1919] p-[12px] rounded-[10px] mr-[15px] hover:shadow-[0_0_20px_rgba(225,0,0,0.5)] hover:cursor-pointer outline-transparent hover:outline-[#FD0408] outline-[1px] transition-all duration-[0.3s]">
+            <img src="/exit-icon.svg" alt="exit" className="flex" />
+        </div>
+    );
+}
+
+export default ButtonExit;
---
+++ frontend/src/ButtonSave.tsx
@@ -0,0 +1,9 @@
+function ButtonSave() {
+    return (
+        <div className="flex h-fit bg-[#275059] p-[12px] rounded-[10px] mr-[15px] hover:shadow-[0_0_20px_rgba(157,255,217,0.5)] hover:cursor-pointer outline-transparent hover:outline-[#04FDE0] outline-[1px] transition-all duration-[0.3s]">
+            <img src="/save-icon.svg" alt="save" className="flex" />
+        </div>
+    );
+}
+
+export default ButtonSave;
