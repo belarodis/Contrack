60dbaaa9fd8ff0f92416447f0d0bb67b472eb87b
HMarchiori <hmarchiori2003@gmail.com>
2025-09-24 22:00:37 -0300

    FEAT: Add Buttons.

--- frontend/src/ButtonPlus.tsx
+++ frontend/src/ButtonPlus.tsx
@@ -1,5 +1,5 @@
 function ButtonPlus() {
-  return (
+    return (
     <div className="flex h-fit bg-[#275059] p-[12px] rounded-[10px] mr-[15px] hover:shadow-[0_0_20px_rgba(157,255,217,0.5)] hover:cursor-pointer outline-transparent hover:outline-[#04FDE0] outline-[1px] transition-all duration-[0.3s]">
       <img src="/plus-icon.svg" alt="plus" className="flex" />
     </div>
