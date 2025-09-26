d8850c0c34298b539f6a5810e622a5f1f91dc81a
Henrique-Schultz <valente.schultz@gmail.com>
2025-09-26 13:45:54 -0300

    adição de icons

--- frontend/src/Alocacao.tsx
+++ frontend/src/Alocacao.tsx
@@ -4,12 +4,19 @@ type AlocacaoProps = {
   tipoPerfil: string;
 };
 
-function Alocacao({nomePessoa, tipoPerfil, horasSemana} : AlocacaoProps) {
+function Alocacao({ nomePessoa, tipoPerfil, horasSemana }: AlocacaoProps) {
   return (
     <div className="flex flex-col bg-[#0D3445] h-fit w-full rounded-[2vh] text-white py-[15px] px-[20px] font-semibold">
-      <h3 className="text-[22px] font-bold">{nomePessoa}</h3>
-          <p className="text-[14px] font-medium pb-[3px]">{`${horasSemana}h/semana`}</p>
-            <p className="text-[14px] font-light">{tipoPerfil.toUpperCase()}</p>
+      <div className="flex items-center gap-[10px]">
+        <img
+          src="/alocacao-icon.svg"
+          alt="plus"
+          className="flex w-auto h-[18px]"
+        />
+        <h3 className="text-[22px] font-bold">{nomePessoa}</h3>
+      </div>
+      <p className="text-[14px] font-medium pb-[3px]">{`${horasSemana}h/semana`}</p>
+      <p className="text-[14px] font-light">{tipoPerfil.toUpperCase()}</p>
     </div>
   );
 }
