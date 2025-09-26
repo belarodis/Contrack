eb871aad4043be379ea77d5043ed4037f3d2b69e
HMarchiori <hmarchiori2003@gmail.com>
2025-09-26 10:48:59 -0300

    style: QoL changes to landing page.

--- frontend/src/Alocacao.tsx
+++ frontend/src/Alocacao.tsx
@@ -9,7 +9,7 @@ function Alocacao({nomePessoa, tipoPerfil, horasSemana} : AlocacaoProps) {
     <div className="flex flex-col bg-[#0D3445] h-fit w-full rounded-[2vh] text-white py-[15px] px-[20px] font-semibold">
       <h3 className="text-[22px] font-bold">{nomePessoa}</h3>
           <p className="text-[14px] font-medium pb-[3px]">{`${horasSemana}h/semana`}</p>
-            <p className="text-[14px] font-light text-shadow-cyan-200">{tipoPerfil.toUpperCase()}</p>
+            <p className="text-[14px] font-light">{tipoPerfil.toUpperCase()}</p>
     </div>
   );
 }
